b1f0ab84500116ecfb2587e4a444c65580ca8b1b
ars-1933 <aklo92@icloud.com>
2021-05-25 15:59:24 +0900

    first commit

---
+++ blog/routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('posts', 'PostController');
+
+Route::resource('comments', 'CommentController');
+
+Route::post('posts/{post}/favorites', 'FavoriteController@store')->name('favorites');
+
+Route::post('posts/{post}/unfavorites', 'FavoriteController@destroy')->name('unfavorites');
